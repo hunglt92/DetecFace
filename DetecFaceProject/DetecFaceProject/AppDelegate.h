7010b53417349f0828d561465f48b4d8c33d74d6
Hung FPT <hungtrong@Hungs-MacBook-Pro.local>
2013-06-17 20:00:04 +0700

    #1 Setup the Project This version 0.1

---
+++ DetecFaceProject/DetecFaceProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DetecFaceProject
+//
+//  Created by Hung FPT on 6/17/13.
+//  Copyright (c) 2013 Hung FPT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DetecFaceProject/DetecFaceProjectTests/DetecFaceProjectTests.m
@@ -0,0 +1,36 @@
+//
+//  DetecFaceProjectTests.m
+//  DetecFaceProjectTests
+//
+//  Created by Hung FPT on 6/17/13.
+//  Copyright (c) 2013 Hung FPT. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DetecFaceProjectTests : XCTestCase
+
+@end
+
+@implementation DetecFaceProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
